1b48f6d1dc2b18f4d5f4b69db4f54a84607dfe18
rehansp <patelrehan37@gmail.com>
2021-11-08 17:19:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Demo from './components/demo';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Demo />
+    </div>
+  );
+}
+
+export default App;
